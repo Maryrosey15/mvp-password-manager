71ee214902fc24f060eceef3d77e48fbfb56fe4f
Mary Garcia <rose.pajanel@yahoo.com>
2024-04-16 13:35:35 +1200

    set up

---
+++ database_table.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Connected to database successfully")
+
+conn.execute('CREATE TABLE accounts (email TEXT, username TEXT, password TEXT, website TEXT, image BLOB)')
+print("Created table successfully")
+
+conn.close()
+
+
+conn = sqlite3.connect('database.db')
+print("Connected to database successfully")
+
+conn.execute('CREATE TABLE users (email TEXT, username TEXT, masterpassword TEXT)')
+print("Created table successfully")
+
+conn.close()
