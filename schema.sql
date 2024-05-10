34b0fd2c8017daa7a86042b8a72cbe0e0d5a7f18
Mary Garcia <rose.pajanel@yahoo.com>
2024-05-10 16:43:21 +1200

    register

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    master_password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    website TEXT NOT NULL,
+    image BLOB NOT NULL
+);
