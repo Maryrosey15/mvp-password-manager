6843b66eac2366b350b643d05bb8fe303abb5dda
Mary Garcia <rose.pajanel@yahoo.com>
2024-06-03 14:39:50 +1200

    forgot and reset

--- app/models.py
+++ app/models.py
@@ -1,18 +1,21 @@
 import sqlite3
 from flask_login import UserMixin
-from flask import g
+from flask import g, session
 import string
 import secrets
+
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def close_db_connection():
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 def get_user_by_username(username):
     db = get_db_connection()
@@ -21,6 +24,22 @@ def get_user_by_username(username):
     db.close()
     return user
 
+
+def get_user_by_email(email):
+    db = get_db_connection()
+    cursor = db.execute('SELECT * FROM users WHERE email = ?', (email,))
+    user = cursor.fetchone()
+    db.close()
+    return user
+
+
+def update_user_password(email, hashed_password):
+    db = get_db_connection()
+    db.execute('Update users SET master_password = ? WHERE email = ?', (hashed_password, email))
+    db.commit()
+    db.close()
+
+
 def init_db():
     db = get_db_connection()
     with open('schema.sql', mode='r') as f:
@@ -28,11 +47,13 @@ def init_db():
     db.commit()
     db.close()
 
+
 class User(UserMixin):
     def __init__(self, id, username, master_password):
         self.id = id
         self.username = username
         self.master_password = master_password
+        session['user_id'] = id
 
     @staticmethod
     def get_by_id(user_id):
