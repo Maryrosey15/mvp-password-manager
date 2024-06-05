617ed279dba08e52b24ee3f6c39e97d1406d12c3
Mary Rose Garcia <92634374+Maryrosey15@users.noreply.github.com>
2024-06-05 14:59:45 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'xxxxx'
     app.config['MAIL_PASSWORD'] = 'xxxxx'
-    app.config['MAIL_DEFAULT_SENDER'] = 'rose.pajanel@gmail.com'
+    app.config['MAIL_DEFAULT_SENDER'] = 'xxxxx'
     mail = Mail(app)
     mail.init_app(app)
     return app
